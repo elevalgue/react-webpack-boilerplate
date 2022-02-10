94e80c25083fe384f4518c703f9ba341f6068136
elena <elena.valenzuela.guerrero@gmail.com>
2022-02-10 16:51:01 +0100

    Add root to our entry point

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,7 @@
-console.log("Hello from JS");
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./app";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root"));
